d6ba8f9f05c381a59885d9d5100a724f3036a744
Li Hiu Nam <65704114+hilariali@users.noreply.github.com>
2025-07-13 13:17:37 +0800

    Create init.py

---
+++ pages/init.py
@@ -0,0 +1,23 @@
+"""
+PDF Manager Pages Module
+
+Contains all page modules for the PDF Manager application.
+"""
+
+from . import (
+    conversion_page,
+    editing_page,
+    organization_page,
+    annotation_page,
+    security_page,
+    ocr_page
+)
+
+__all__ = [
+    'conversion_page',
+    'editing_page', 
+    'organization_page',
+    'annotation_page',
+    'security_page',
+    'ocr_page'
+]
