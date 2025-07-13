0963b923cea0ac45723f09e49ed5efe0025faa2e
Li Hiu Nam <65704114+hilariali@users.noreply.github.com>
2025-07-13 13:17:57 +0800

    Create init.py

---
+++ core/init.py
@@ -0,0 +1,8 @@
+"""
+Core functionality for PDF Manager application
+"""
+
+from .session_manager import SessionManager
+from .ui_components import UIComponents
+
+__all__ = ['SessionManager', 'UIComponents']
